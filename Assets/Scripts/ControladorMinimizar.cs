18ca6c94e51d5087b598a1a5a49361d0fdf6432d
LamenLuan <luan.leme.s35@gmail.com>
2021-10-16 13:53:20 -0300

    Add minimize button to all scenes, and deleting unused images

---
+++ Assets/Scripts/ControladorMinimizar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ControladorMinimizar : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    public void Minimizar()
+    {
+        ShowWindow(GetActiveWindow(), 2);
+    }
+
+}
