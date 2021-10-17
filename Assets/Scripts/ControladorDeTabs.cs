85b550f876669c237ac0b3faca828c8a051c31a0
LamenLuan <luan.leme.s35@gmail.com>
2021-10-17 16:42:25 -0300

    Now, selects the first field when pressing tab on the last one

--- Assets/Scripts/ControladorDeTabs.cs
+++ Assets/Scripts/ControladorDeTabs.cs
@@ -4,31 +4,33 @@ using UnityEngine.UI;
 public class ControladorDeTabs : MonoBehaviour
 {
     private EventSystem system;
+    [SerializeField] private GameObject firstFieldObj;
     
-    // Start is called before the first frame update
-    void Start()
+    void Start() // Start is called before the first frame update
     {
         system = EventSystem.current;
     }
 
-    // Update is called once per frame
-    void Update() {
+    void Update() { // Update is called once per frame
         if( Input.GetKeyDown(KeyCode.Tab) ) {
 
             GameObject gameObj = system.currentSelectedGameObject;
+            
             if(gameObj != null) {
                 Selectable prox = gameObj.GetComponent<Selectable>();
                 prox = prox.FindSelectableOnDown();
             
-                if(prox!= null) {         
-                    InputField inputfield = prox.GetComponent<InputField>();
-                    if (inputfield !=null)
-                        inputfield.OnPointerClick( new PointerEventData(system) );
-                                
-                    system.SetSelectedGameObject(
-                        prox.gameObject, new BaseEventData(system)
-                    );
-                }
+                // If there's not a next field below, select the first one
+                if(prox == null)
+                    prox = firstFieldObj.GetComponent<Selectable>();
+    
+                InputField inputfield = prox.GetComponent<InputField>();
+                if (inputfield != null)
+                    inputfield.OnPointerClick( new PointerEventData(system) );
+                            
+                system.SetSelectedGameObject(
+                    prox.gameObject, new BaseEventData(system)
+                );
             }
         }
     }
