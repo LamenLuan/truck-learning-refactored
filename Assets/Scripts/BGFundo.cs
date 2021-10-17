414318da571aa5d9e59270ca7b67625fe89d71a1
LamenLuan <luan.leme.s35@gmail.com>
2021-10-17 10:16:10 -0300

    Organizing stuff

--- Assets/Scripts/BGFundo.cs
+++ Assets/Scripts/BGFundo.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class BGFundo : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject botaooff, botaoon;
+    [SerializeField] private GameObject botaooff, botaoon;
 
     void Start()
     {
-        if (AudioManager.instance.musicaBG.isPlaying)
-        {
+        if (AudioManager.instance.musicaBG.isPlaying) {
             botaooff.SetActive(false);
             botaoon.SetActive(true);
 
-        }else
-        {
+        } else {
             botaooff.SetActive(true);
             botaoon.SetActive(false);
         }
-        
     }
 
     public void PauseSound()
     {
        AudioManager.instance.SonsBGToca(botaooff, botaoon);
-              
     }
 
     public void Destruir()
