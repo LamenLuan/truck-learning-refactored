7c4e46bfd276df08e613512ddc5aac390900f1f1
LamenLuan <luan.leme.s35@gmail.com>
2021-12-04 17:21:19 -0300

    Dinamicaly associating component to avoid manual reassociations

--- Assets/Scripts/NuvemScroll.cs
+++ Assets/Scripts/NuvemScroll.cs
@@ -3,9 +3,14 @@
 public class NuvemScroll : MonoBehaviour
 {
     [SerializeField] private float _velocidade = 0.05f;
-    [SerializeField] private Renderer _renderer;
+    private Renderer _renderer;
     
-    void Update() // Update is called once per frame
+    void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
     {
         Vector2 offset = new Vector2(_velocidade * Time.deltaTime, 0);
         _renderer.material.mainTextureOffset -= offset;
